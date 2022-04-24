724d33ee5faa217190d1b339208f944f2b928344
dol23asuka <dol23@student.ubc.ca>
2022-04-23 22:19:43 -0700

    Add app.py and model.joblib

---
+++ src/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+## Import any other packages that are needed
+
+app = Flask(__name__)
+
+# 1. Load your model here
+model = joblib.load('model.joblib')
+
+# 2. Define a prediction function
+def return_prediction(data_input):
+
+    # format input_data here so that you can pass it to model.predict()
+    data_input = np.array(data_input)
+    return model.predict([data_input])
+
+# 3. Set up home page using basic html
+@app.route("/")
+def index():
+    # feel free to customize this if you like
+    return """
+    <h1>Welcome to our rain prediction service</h1>
+    To use this service, make a JSON post request to the /predict url with 25 climate model outputs.
+    """
+
+# 4. define a new route which will accept POST requests and return model predictions
+@app.route('/predict', methods=['POST'])
+def rainfall_prediction():
+    content = request.json  # this extracts the JSON content we sent
+    prediction = return_prediction(content['data'])
+    results = {
+        "Input": content['data'],
+        "Prediction": prediction[0]
+    }  # return whatever data you wish, it can be just the prediction
+       # or it can be the prediction plus the input data, it's up to you
+    return jsonify(results)
